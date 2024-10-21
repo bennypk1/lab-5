e8f6b6f0ac0275a4d00da820ec54bd7977654252
bennypk1 <b.cumminsmburu@mail.utoronto.ca>
2024-10-21 16:22:22 -0400

    Completed the code for LogoutInputData and LogoutInteractor

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -4,22 +4,29 @@ package use_case.logout;
  * The Logout Interactor.
  */
 public class LogoutInteractor implements LogoutInputBoundary {
-    private LogoutUserDataAccessInterface userDataAccessObject;
-    private LogoutOutputBoundary logoutPresenter;
+
+    private final LogoutUserDataAccessInterface userDataAccessObject;
+    private final LogoutOutputBoundary logoutPresenter;
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
+        // TODO: save the DAO and Presenter in the instance variables - Completed by Benedict
         // Which parameter is the DAO and which is the presenter?
+        this.logoutPresenter = logoutOutputBoundary;
+        this.userDataAccessObject = userDataAccessInterface;
     }
 
     @Override
     public void execute(LogoutInputData logoutInputData) {
-        // TODO: implement the logic of the Logout Use Case (depends on the LogoutInputData.java TODO)
+        // TODO: implement the logic of the Logout Use Case - Completed by Benedict
         // * get the username out of the input data,
+        final String username = logoutInputData.getCurrentUsername();
         // * set the username to null in the DAO
+        this.userDataAccessObject.setCurrentUsername(username);
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
         // * tell the presenter to prepare a success view.
+        this.logoutPresenter.prepareSuccessView(logoutOutputData);
     }
 }
 
